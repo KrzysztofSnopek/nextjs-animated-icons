4182a46186b37d51f1b0318874ebf813d3ad1774
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-09-25 16:35:31 +0200

    Adds before and after effects to get shadow on the card and adds transform to the title element

--- src/app/Card/CharacterCard.tsx
+++ src/app/Card/CharacterCard.tsx
@@ -36,13 +36,16 @@ export default function CharacterCard(cardProps: cardProps) {
           heightVariants[cardProps.cardData.height]
         } items-end justify-center px-8`}
       >
-        <div className="wrapper card-transition group-hover:wrapper-transform absolute -z-10 w-full">
+        <div className="wrapper card-transition group-hover:wrapper-transform before:wrapper-transition after:wrapper-transition before:wrapper-shadow-before after:wrapper-shadow-after absolute -z-10 w-full group-hover:before:opacity-100 group-hover:after:h-60 group-hover:after:opacity-100">
           <img
             src={cardProps.cardData.bgImageSrc}
             className="cover-image h-full w-full object-cover"
           />
         </div>
-        <img src={cardProps.cardData.titleSrc} className="title" />
+        <img
+          src={cardProps.cardData.titleSrc}
+          className="title transition-title group-hover:transform-title group-hover:shadow-xl"
+        />
         <img
           src={cardProps.cardData.characterImageSrc}
           className="character card-transition group-hover:character-transform absolute -z-10 w-full opacity-0"
